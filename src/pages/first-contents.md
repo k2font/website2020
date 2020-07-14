28d1bf5cb7b14cdd738163612fbad2a991c88669
Shoichiro Kono <shoichiro.kouno@gmail.com>
2020-07-14 08:50:21 +0000

    Initial commit

---
+++ src/pages/first-contents.md
@@ -0,0 +1,12 @@
+---
+title: "最初の投稿"
+date: "2020-09-20"
+---
+
+## 概要
+はじめまして。これは最初の投稿です
+
+### 題名
+文字
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>
---
+++ src/pages/index.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+
+export default function Home({ data }) {
+  console.log(data)
+  return (
+    <Layout>
+      <div>
+        <h1 css={css`
+          display: inline-block;
+          border-bottom: 1px solid;
+        `}>
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.title}{""}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  - {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+              <p>文字数: {node.wordCount.words}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+      totalCount
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          wordCount {
+            paragraphs
+            sentences
+            words
+          }
+        }
+      }
+    }
+  }
+`
